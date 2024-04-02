96afaa1aca39eceae79f5071a3bf9e51f8c47ca5
Kevin-Tuco <kevin_tuco@dlsu.edu.ph>
2024-04-02 22:32:59 +0800

    Update main.c

--- x86_64_Hola1/x86_64_Hola1/main.c
+++ x86_64_Hola1/x86_64_Hola1/main.c
@@ -14,11 +14,7 @@ int main() {
     double A = 1.0 + rand() / (RAND_MAX / 9.0);
 
     // Variable for the size of arrays X and Y
-    // 2^20 =    1048576
-    // 2^24 =   16777216
-    // 2^27 =  134217728
-    // 2^28 =  268435456 Can't Handle
-    int n = 134217728; // Example size, you can set it according to your needs
+    int n = 1048576; // Example size, you can set it according to your needs
 
     // Create arrays X and Y with size n dynamically
     double* X = malloc(n * sizeof(double));
@@ -45,22 +41,34 @@ int main() {
     printf("\n");
     printf("Y: ");
     for (int i = 0; i < n && i < print_limit; i++) {
-        printf("%.6f ", X[i]);
+        printf("%.6f ", Y[i]);
     }
     printf("\n\n-----------------------------\n");
-    // Call the daxpy function from the "CKernel" program and measure execution time
-    clock_t C_start_time = clock();
-    double* OutC = daxpyC(n, A, X, Y);
-    clock_t C_end_time = clock();
-    double C_execution_time = (double)(C_end_time - C_start_time) / CLOCKS_PER_SEC;
+
+    // Measure execution time in C
+    volatile double* OutC; // Declare as volatile to prevent optimizations
+    LARGE_INTEGER C_start_time, C_end_time, C_frequency;
+    QueryPerformanceFrequency(&C_frequency);
+    QueryPerformanceCounter(&C_start_time);
+    OutC = daxpyC(n, A, X, Y);
+    QueryPerformanceCounter(&C_end_time);
+    double C_execution_time = (double)(C_end_time.QuadPart - C_start_time.QuadPart) / C_frequency.QuadPart;
     printf("Execution time in C: %.6f seconds\n", C_execution_time);
 
-    // Call the daxpy function from the "asmKernel" program and measure execution time
-    clock_t Asm_start_time = clock();
-    double* OutAsm = daxpyAsm(n, A, X, Y);
-    clock_t Asm_end_time = clock();
-    double Asm_execution_time = (double)(Asm_end_time - Asm_start_time) / CLOCKS_PER_SEC;
+    // Measure execution time in Assembly
+    volatile double* OutAsm; // Declare as volatile to prevent optimizations
+    LARGE_INTEGER Asm_start_time, Asm_end_time;
+    QueryPerformanceCounter(&Asm_start_time);
+    OutAsm = daxpyAsm(n, A, X, Y);
+    QueryPerformanceCounter(&Asm_end_time);
+    double Asm_execution_time = (double)(Asm_end_time.QuadPart - Asm_start_time.QuadPart) / C_frequency.QuadPart;
     printf("Execution time in Assembly: %.6f seconds\n", Asm_execution_time);
-    
+
+    // Free dynamically allocated memory
+    free(X);
+    free(Y);
+    free(OutC);
+    free(OutAsm);
+
     return 0;
-};
+}
